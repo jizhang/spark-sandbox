5ec484dcf87e4039feeebd4526632c0a927cdaf7
Ji ZHANG <jizhang@anjuke.com>
2015-07-02 18:23:09 +0800

    recommend for user

--- src/main/scala/CosineSimilarity.scala
+++ src/main/scala/CosineSimilarity.scala
@@ -13,6 +13,7 @@ object CosineSimilarity {
     val conf = new SparkConf().setAppName("CosineSimilarity").setMaster("local[2]")
     val sc = new SparkContext(conf)
 
+    // read logs
     val logs = sc.textFile("/Users/jizhang/data/comm.txt").flatMap { line =>
       try {
         val arr = line.split("\\s+")
@@ -25,22 +26,53 @@ object CosineSimilarity {
     val numColumns = logs.map(_._2).max + 1
     println(s"numColumns = $numColumns")
 
-    val rows = logs.map { case (userId, commId, clicks) =>
+    val userComms = logs.map { case (userId, commId, clicks) =>
       (userId, Seq(commId -> clicks.toDouble))
-    }.reduceByKey(_ ++ _).values.map { commClicks =>
+    }.reduceByKey(_ ++ _).cache()
+
+    val rows = userComms.values.map { commClicks =>
       Vectors.sparse(numColumns, commClicks)
     }
 
     val mat = new RowMatrix(rows)
-    val sim = mat.columnSimilarities(0.1)
+    val sim = mat.columnSimilarities(0.1).toIndexedRowMatrix
 
-    sim.entries.filter(entry => Set(253147, 251367).contains(entry.i.toInt))
-      .map(entry => (entry.i, Seq(entry.j -> entry.value)))
-      .reduceByKey(_ ++ _)
-      .foreach { case (i, items) =>
-        val itemList = items.sortWith(_._2 > _._2).take(5)
-        println(i + ": " + itemList.mkString(", "))
-      }
+    sim.rows.take(10).foreach { row =>
+      val items = (0 until row.vector.size).map(i => i -> row.vector(i)).filter(_._2 > 0).sortWith(_._2 > _._2).take(100)
+      println(row.index + ": " + items.mkString(", "))
+    }
+
+    userComms.filter(_._1 == "000b63c9ea5a4b4840663f2ea5b59635").take(1).foreach { case (userId, comms) =>
+
+      val l2norm = math.sqrt(comms.map(t => t._2 * t._2).sum)
+      val l2normComms = comms.map(t => t._1 -> t._2 / l2norm)
+
+      println(l2normComms)
+
+      val commIds = l2normComms.map(_._1).toSet
+      val matches = sim.rows.filter(row => commIds.contains(row.index.toInt))
+        .map { row =>
+          val items = (0 until row.vector.size).map(i => i -> row.vector(i))
+            .filter { case (commId, similarity) => similarity > 0 && !commIds.contains(commId) }
+          (row.index, items.toMap)
+        }
+        .collectAsMap()
+
+      println(matches.mapValues(_.take(100)))
+
+      val recommendations = l2normComms.flatMap { case (commId, clicks) =>
+        matches.getOrElse(commId, Map.empty).mapValues(similarity => (similarity, clicks))
+      }.groupBy(_._1).mapValues(_.map(_._2)).mapValues { items =>
+        val score = items.map(item => item._1 * item._2).sum
+        val total = items.map(_._1).sum
+        (score / total, score, total)
+      }.toSeq.sortWith(_._2._1 > _._2._1)
+
+      println(recommendations.size)
+
+      recommendations.take(100).foreach(println)
+
+    }
 
     sc.stop()
   }
