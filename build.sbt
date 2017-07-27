98d55faa15f1828230b47130450e778f6d74e5c8
Ji ZHANG <zhangji87@gmail.com>
2017-07-27 15:22:15 +0800

    fix #1 upgrade to spark 2.2

--- build.sbt
+++ build.sbt
@@ -4,10 +4,10 @@ organization := "com.shzhangji"
 
 version := "0.1.0"
 
-scalaVersion := "2.10.5"
+scalaVersion := "2.11.8"
 
 libraryDependencies ++= {
-  val sparkVersion = "1.5.1"
+  val sparkVersion = "2.2.0"
   Seq(
     "org.scalatest" %% "scalatest" % "2.2.1" % "test",
     "com.github.scopt" %% "scopt" % "3.3.0",
@@ -15,13 +15,7 @@ libraryDependencies ++= {
     "org.apache.spark" %% "spark-mllib" % sparkVersion % "provided",
     "org.apache.spark" %% "spark-graphx" % sparkVersion % "provided",
     "org.apache.spark" %% "spark-streaming" % sparkVersion % "provided",
-    "org.apache.spark" %% "spark-streaming-kafka" % sparkVersion intransitive(),
-    ("org.apache.kafka" %% "kafka" % "0.8.2.1").
-      exclude("com.sun.jmx", "jmxri").
-      exclude("com.sun.jdmk", "jmxtools").
-      exclude("net.sf.jopt-simple", "jopt-simple").
-      exclude("org.slf4j", "slf4j-simple").
-      exclude("org.apache.zookeeper", "zookeeper")
+    "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion
   )
 }
 
--- src/main/scala/DirectKafka.scala
+++ src/main/scala/DirectKafka.scala
@@ -1,27 +1,34 @@
-import kafka.serializer.StringDecoder
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark._
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.kafka._
+import org.apache.spark.streaming.kafka010._
 
 object DirectKafka {
 
   def main(args: Array[String]): Unit = {
 
     val Array(brokers, topics) = args
-    val kafkaParams = Map("bootstrap.servers" -> brokers)
+    val kafkaParams = Map[String, Object](
+        "bootstrap.servers" -> brokers,
+        "key.deserializer" -> classOf[StringDeserializer],
+        "value.deserializer" -> classOf[StringDeserializer],
+        "group.id" -> "spark-sandbox",
+        "auto.offset.reset" -> "latest")
     val topicSet = topics.split(",").toSet
 
     val conf = new SparkConf().setAppName("DirectKafka").setIfMissing("spark.master", "local[2]")
     val ssc = new StreamingContext(conf, Seconds(3))
 
     var offsetRanges = Array.empty[OffsetRange]
-    val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicSet)
+    val messages = KafkaUtils.createDirectStream[String, String](ssc,
+        LocationStrategies.PreferConsistent,
+        ConsumerStrategies.Subscribe[String, String](topicSet, kafkaParams))
       .transform { rdd =>
         offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
         rdd
       }
 
-    messages.flatMap(_._2.split(" ")).map(_ -> 1).reduceByKey(_ + _).foreachRDD { (rdd, time) =>
+    messages.flatMap(_.value.split(" ")).map(_ -> 1).reduceByKey(_ + _).foreachRDD { (rdd, time) =>
       println("Time: " + time)
       offsetRanges.foreach(println)
       rdd.foreach(println)
